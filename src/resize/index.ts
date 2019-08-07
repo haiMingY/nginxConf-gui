90de5595a5f5ec034a2adf94c64c9c82a4160a7b
luodl <826769387@qq.com>
2019-08-07 15:48:45 +0800

    修改了 resize

--- src/resize/index.ts
+++ src/resize/index.ts
@@ -3,7 +3,7 @@
  * @param {Function} cb
  * @param {Number} wait
  */
-function debounce(cb, delay = 100) {
+function debounce(cb: Function, delay = 100) {
     let timer: any = null
     let result: any = null
     let context: any, args: any
@@ -25,6 +25,46 @@ function debounce(cb, delay = 100) {
     }
 }
 
-const controller = (function (params) {
-    return {}
+function isDef() {
+    return Object.prototype.hasOwnProperty.call(window, 'MutationObserver')
+}
+
+
+interface result {
+    add: Function,
+    remove: Function,
+}
+
+const controller = (function (params): result {
+    let cache: Array<Resize> = []
+    return {
+        add(resize: Resize) {
+            return cache.push(resize)
+        },
+        remove(resize: Resize) {
+            let i = cache.indexOf(resize)
+            if (i >= 0) cache.splice(i, 1)
+        }
+    }
 })()
+
+
+function init() {
+    let cb = debounce(Listener)
+    window.addEventListener("resize", cb)
+
+    let observer = new MutationObserver(cb)
+    observer.observe(document, {})
+}
+
+
+function Listener() {
+
+}
+
+
+class Resize {
+    constructor() {
+        controller.add(this)
+    }
+}
